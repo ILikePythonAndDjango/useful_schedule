6d81211528aa3bd40ba766793a07c5661966695f
ILikePythonAndDjango <nik1995osipov@gmail.com>
2018-07-01 23:06:03 +0300

    akdjf;kl\

--- core/models.py
+++ core/models.py
@@ -13,12 +13,12 @@ class Goal(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tag = models.ForeignKey(TypeGoal, on_delete=models.SET_NULL)
+    tag = models.ForeignKey(TypeGoal, on_delete=models.SET_NULL, blank=True, null=True)
     begin = models.DateField(auto_now_add=True)
     deadline = models.DateField()
     is_done = models.BooleanField(default=False)
-    sub_goals = models.ForeignKey('Goal', on_delete=models.SET_NULL, blank=True, null=True)
-    cost = models.IntegerField(blank=True, null=None)
+    sub_goals = models.ManyToManyField('Goal', blank=True, null=True)
+    cost = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.title
