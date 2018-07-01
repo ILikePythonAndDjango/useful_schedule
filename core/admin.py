3febac4266838b6b4464d883e2b80a29865cd51b
ILikePythonAndDjango <nik1995osipov@gmail.com>
2018-07-01 21:55:19 +0300

    make Goal's model with .gitignore

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TypeGoal, Goal
 
-# Register your models here.
+#Goals
+admin.site.register(TypeGoal)
+admin.site.register(Goal)
--- core/models.py
+++ core/models.py
@@ -3,12 +3,21 @@ from django.contrib.auth.models import User
 
 class TypeGoal(models.Model):
 
-    title = models.CharField(max_length)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 class Goal(models.Model):
 
     title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.ForeignKey(User)
-    tag = models.ForeignKey(TypeGoal)
-    begin = 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ForeignKey(TypeGoal, on_delete=models.CASCADE)
+    begin = models.DateField(auto_now_add=True)
+    deadline = models.DateField()
+    is_done = models.BooleanField(default=False)
+    sub_goals = models.ForeignKey('Goal', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
